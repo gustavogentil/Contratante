200c4639d1e24ea0ea38ff532d1c2b43dc4427b5
gustavo gentil <gustavogentil_sp@hotmail.com>
2020-11-28 18:56:52 -0300

    primeiro commit

---
+++ src/main/java/com/example/contratante/Contratante.java
@@ -0,0 +1,13 @@
+package com.example.contratante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Contratante {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Contratante.class, args);
+	}
+
+}
